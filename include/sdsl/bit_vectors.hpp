7be8bbc2a19f79e926624cea21d70be0cfcacd62
Simon Gog <simon.gog@gmail.com>
2012-04-10 20:45:12 +1000

    Added header file which contains all bit_vector implementations

---
+++ include/sdsl/bit_vectors.hpp
@@ -0,0 +1,15 @@
+/*! \file bit_vectors.hpp
+    \brief bit_vectors.hpp contains classes for uncompressed and compressed bit vector representations.
+	\author Simon Gog
+*/
+#ifndef INCLUDED_SDSL_BITVECTORS
+#define INCLUDED_SDSL_BITVECTORS
+
+#include "int_vector.hpp"
+#include "bit_vector_interleaved.hpp"
+#include "rrr_vector.hpp"
+#include "rrr_vector_var.hpp"
+#include "sd_vector.hpp"
+#include "gap_vector.hpp"
+
+#endif
