9d1cbed1e28fab7daa27a38b70f42fddd169fca9
Simon Gog <simon.gog@unimelb.edu.au>
2012-08-04 19:40:55 +1000

    Now the Makefile gets configured by CMake

--- examples/Makefile.cmake
+++ examples/Makefile.cmake
@@ -1,5 +1,5 @@
 CC=g++
-CC_FLAGS=-Wall -g -O3 -I${HOME}/include -L${HOME}/lib -DNDEBUG -funroll-loops -msse4.2
+CC_FLAGS=-Wall -g -O3 -I@CMAKE_INSTALL_PREFIX@/include -L@CMAKE_INSTALL_PREFIX@/lib -DNDEBUG -funroll-loops -msse4.2
 CCLIB=-lsdsl -ldivsufsort -ldivsufsort64 
 SOURCES=$(wildcard *.cpp)
 EXECS=$(SOURCES:.cpp=)
