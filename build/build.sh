872993dce11c9ace7b0445f8c07e20abf518dfa3
Simon Gog <simon.gog@unimelb.edu.au>
2012-08-08 17:12:55 +1000

    Now it is possible to run this from everywhere

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+CUR_DIR=`pwd`
+OLD_DIR="$( cd "$( dirname "$0" )" && pwd )" # gets the directory where the script is located in
+cd "${OLD_DIR}"
 
 cmake .. -DCMAKE_INSTALL_PREFIX=${HOME} && make install
+
+cd ${CUR_DIR}
