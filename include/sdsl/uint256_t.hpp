fb5c40d952e36171fac1776ba6c01de58371d208
Simon Gog <simon.gog@unimelb.edu.au>
2012-10-03 14:32:50 +1000

    Improved rrr_vector_15 and fixed uintX output
    * We use only 2x64kB lookup tables for rrr_vector_15 which are
      also hugetable mappable, instead of 2x128kB.

--- include/sdsl/uint256_t.hpp
+++ include/sdsl/uint256_t.hpp
@@ -232,17 +232,7 @@ class uint256_t
         }
 };
 
-std::ostream& operator<<(std::ostream& os, const uint256_t& x)
-{
-    uint64_t X[4] = {(uint64_t)(x.m_high >> 64), (uint64_t)x.m_high, x.m_mid, x.m_lo};
-    for (int j=0; j < 4; ++j) {
-        for (int i=0; i < 16; ++i) {
-            os << std::hex << ((X[j]>>60)&0xFULL) << std::dec;
-            X[j] <<= 4;
-        }
-    }
-    return os;
-}
+std::ostream& operator<<(std::ostream& os, const uint256_t& x);
 
 } // end namespace
 
