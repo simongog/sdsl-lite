d9ce893a274a073f5b3ec8bb92b8a39dfde4861a
Simon Gog <simon.gog@gmail.com>
2012-10-18 09:46:37 +1100

    Added method to serialize and load vectors.
    In util the methods
      * serialize_vector
      * load_vector
    were added. The methods take a std::vector
    of sdsl-objects and serialized/loads each
    object. The size of the vector is not
    stored. So before calling load_vector, the
    vector has to be resized to its original size.
    
    If the objects have all the same structure tree,
    then the methods merges all the structure trees
    by summing up the sizes of the components.

--- include/sdsl/structure_tree.hpp
+++ include/sdsl/structure_tree.hpp
@@ -5,10 +5,12 @@
 #ifndef INCLUDED_SDSL_STRUCTURE_TREE
 #define INCLUDED_SDSL_STRUCTURE_TREE
 
+#include "uintx_t.hpp"
 #include <vector>
 #include <map>
 #include <string>
 #include <iostream>
+#include <sstream>
 
 using std::vector;
 using std::map;
@@ -17,6 +19,10 @@ using std::string;
 //! Namespace for the succinct data structure library
 namespace sdsl
 {
+
+static const char STRUCTURE_TREE_SIZE_KEY[] = "size";
+static const char STRUCTURE_TREE_NAME_KEY[] = "name";
+static const char STRUCTURE_TREE_CLASS_NAME_KEY[] = "class_name";
 
 class structure_tree; // forward declaration
 
@@ -39,6 +45,8 @@ class structure_tree_node
         vector<structure_tree_node*> m_children;
         map<string, string>			 m_key_values;
         void copy(const structure_tree_node& v);
+		void delete_children();
+		bool equal_key_value_pairs(const tKeyValue& kv1, const tKeyValue& kv2)const;
 	public:
 
         structure_tree_node*&			 parent;
@@ -67,17 +75,27 @@ class structure_tree_node
         void swap(structure_tree_node& v);
         //! Add a key value pair.
         void add_key_value(const string& key, const string& value);
-        template<class IntType>
-        void add_size(IntType value);
+        void add_size(uint64_t value);
+
+		//! Adds the size values of structure_tree_node v recursively to that of the current node.
+		/*! \return True, if the recursive structure of both nodes is exactly the same and 
+		 *          the size values were integers. 
+		 */
+		bool merge(const structure_tree_node &v);
+
+	private:
+		bool equal_structure(const structure_tree_node &v)const;
+
+		void rec_merge(const structure_tree_node &v);
 };
 
 class structure_tree
 {
     public:
         static structure_tree_node* add_child(structure_tree_node* v, const string& name, const string& class_name);
-        template<class IntType>
-        static void add_size(structure_tree_node* v, IntType value);
+        static void add_size(structure_tree_node* v, uint64_t value);
         static structure_tree_node* parent(const structure_tree_node* v);
+		static bool merge_children(structure_tree_node *v);
 };
 
 
@@ -86,20 +104,6 @@ enum format_type {JSON_FORMAT, R_FORMAT};
 template<format_type F>
 void write_structure_tree(const structure_tree_node* v, std::ostream& out);
 
-
-template<class IntType>
-void structure_tree_node::add_size(IntType value)
-{
-    m_key_values["size"] = util::to_string(value);
-}
-
-template<class IntType>
-void structure_tree::add_size(structure_tree_node* v, IntType value)
-{
-    if (NULL != v) {
-        v->add_size(value);
-    }
-}
 
 }
 #endif
