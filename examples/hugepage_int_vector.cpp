40965785ada9b7a3cb07878bed42c97660ce61b2
Simon Gog <simon.gog@gmail.com>
2013-03-15 10:31:41 +1100

    Adapted files to renamed bit_vector_il.

--- examples/hugepage_int_vector.cpp
+++ examples/hugepage_int_vector.cpp
@@ -1,25 +1,26 @@
 #include <sdsl/int_vector.hpp>
-#include <sdsl/bit_vector_interleaved.hpp>
+#include <sdsl/bit_vector_il.hpp>
 #include <iostream>
 
 using namespace sdsl;
 using namespace std;
 
-int main(){	
-	{
-		bit_vector v(1ULL<<30);
-		bool mapped = mm::map_hp();
-		cout << v[0] << endl;
-		if ( mapped ){
-		   	mm::unmap_hp();
-		}
-	}
-	{
-		bit_vector_interleaved<> v(1ULL<<30);
-		bool mapped = mm::map_hp();
-		cout << v[0] << endl;
-		if ( mapped ){
-		   	mm::unmap_hp();
-		}
-	}
+int main()
+{
+    {
+        bit_vector v(1ULL<<30);
+        bool mapped = mm::map_hp();
+        cout << v[0] << endl;
+        if (mapped) {
+            mm::unmap_hp();
+        }
+    }
+    {
+        bit_vector_il<> v(1ULL<<30);
+        bool mapped = mm::map_hp();
+        cout << v[0] << endl;
+        if (mapped) {
+            mm::unmap_hp();
+        }
+    }
 }
