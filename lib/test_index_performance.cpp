112581a9b9b0da0dfd3d847c0d513c572719f640
Simon Gog <simon.gog@gmail.com>
2013-03-14 13:57:25 +1100

    Renamed all_elements_mod(v,m) -> mod(v,m)

--- lib/test_index_performance.cpp
+++ lib/test_index_performance.cpp
@@ -9,7 +9,7 @@ int_vector<64> get_rnd_positions(uint8_t log_s, uint64_t& mask, uint64_t mod, ui
     int_vector<64> rands(1<<log_s ,0);
     util::set_random_bits(rands, seed);
     if (mod>0) {
-        util::all_elements_mod(rands, mod);
+        util::mod(rands, mod);
     }
     return rands;
 }
