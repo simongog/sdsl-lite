fbaee6c0fdff69f6ae35820829227f2c802dd708
Simon Gog <simon.gog@gmail.com>
2013-03-14 23:03:50 +1100

    Adapted install.sh to new build structure.

--- install.sh
+++ install.sh
@@ -8,9 +8,7 @@ if [ $# -ge 1 ]; then
 	echo "Library will be installed in ${1}"
 fi
 
-
-sufsort_dir="libdivsufsort-2.0.1"
-gtest_dir="gtest-1.6.0"
+# (1) Copy pre-commit hook
 
 OLD_DIR="$( cd "$( dirname "$0" )" && pwd )" # gets the directory where the script is located in
 cd "${OLD_DIR}"
@@ -30,77 +28,7 @@ else
 	echo "         The pre-commit hook is not installed."
 fi
 
-# (1) Install libdivsufsort-2.0.1
-cd "${sufsort_dir}/build"  # step into the build dir of libdivsufsort
-rm -f CMakeCache.txt
-
-cmake -DCMAKE_INSTALL_PREFIX=${SDSL_INSTALL_PREFIX} -DBUILD_DIVSUFSORT64=ON \
-	  -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE="Release" -DBUILD_EXAMPLES=OFF .. # run cmake 
-if [ $? != 0 ]; then
-	echo "ERROR: libdivsuftsort cmake call failed."
-	exit 1
-fi
-make # run make
-if [ $? != 0 ]; then
-	echo "ERROR: libdivsuftsort build failed."
-	exit 1
-fi
-
-if [ ! -d "${SDSL_INSTALL_PREFIX}/include" ]; then # if there exists not include dir 
-	mkdir "${SDSL_INSTALL_PREFIX}/include"              # try to create one
-fi
-if [ ! -d "${SDSL_INSTALL_PREFIX}/lib" ]; then     #if there exits no lib dir
-	mkdir "${SDSL_INSTALL_PREFIX}/lib"
-fi
-make install  # install libdivsufsort
-if [ $? != 0 ]; then
-	echo "ERROR: libdivsuftsort install failed."
-	exit 1
-fi
-
-cd ../.. # go to the original directory
-if [ "`pwd`" != "${OLD_DIR}" ]; then
-	echo "ERROR: we are not in the original dir ${OLD_DIR} now."
-	exit 1
-fi
-echo "SUCCESS: Yuta Mori's libdivsufsort was installed successfully!"
-rm -rf "${sufsort_dir}/build/*"
-# (2) Install gtest
-
-cd "${gtest_dir}/build"  # step into the build dir of gtest
-rm -f CMakeCache.txt
-
-cmake -DCMAKE_INSTALL_PREFIX=${SDSL_INSTALL_PREFIX} \
-	  -DBUILD_SHARED_LIBS=OFF -Dgtest_disable_pthreads=ON .. 
-if [ $? != 0 ]; then
-	echo "ERROR: gtest cmake call failed."
-	exit 1
-fi
-make # run make
-if [ $? != 0 ]; then
-	echo "ERROR: gtest build failed."
-	exit 1
-fi
-cp -f ./libgtest* "${SDSL_INSTALL_PREFIX}/lib" # copy generated libraries to the destination
-if [ $? != 0 ]; then
-	echo "ERROR: could not copy libgtest* to ${SDSL_INSTALL_PREFIX}/lib"
-	exit 1
-fi
-cp -rf  ../include/gtest "${SDSL_INSTALL_PREFIX}/include"
-if [ $? != 0 ]; then
-	echo "ERROR: could not copy gtest include files to ${SDSL_INSTALL_PREFIX}/include"
-	exit 1
-fi
-
-cd ../.. # go to the original directory
-if [ "`pwd`" != "${OLD_DIR}" ]; then
-	echo "ERROR: we are not in the original dir ${OLD_DIR} now."
-	exit 1
-fi
-echo "SUCCESS: gtest was installed successfully!"
-#rm -rf "${sufsort_dir}/build/*"
-
-# (3) Finally, install sdsl
+# (2) Install divsufsort, gtest, and sdsl
 
 cd build # change into the build directory
 if [ $? != 0 ]; then
