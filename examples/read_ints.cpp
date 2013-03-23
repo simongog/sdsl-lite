ff0e1b3787a6fd8b6e583191209d7b899e0308fe
Simon Gog <simon.gog@gmail.com>
2013-03-23 22:07:47 +1100

    Examples for CST from decimal numbers.

---
+++ examples/read_ints.cpp
@@ -0,0 +1,30 @@
+#include <sdsl/suffix_trees.hpp>
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+using namespace sdsl;
+
+int main()
+{
+    {
+        typedef csa_wt<wt_int<>, 32, 64, sa_order_sa_sampling<>, int_vector<>, int_alphabet_strategy<> > t_csa;
+//		typedef csa_wt<> t_csa;
+        typedef cst_sct3<t_csa> t_cst;
+        t_cst cst;
+        std::streamsize BUF_SIZE=4096;
+        char* text = new char[BUF_SIZE];
+        while (cin.getline(text, BUF_SIZE)) {
+            construct_im(cst, (const char*)text, 'd');
+            printf("   i  SA LCP     T\n");
+            for (uint64_t i=0; i <cst.csa.size(); ++i) {
+                printf("%4lld %3lld %3lld     ", i, cst.csa[i], cst.lcp[i]);
+                for (uint64_t j=cst.csa[i]; j < cst.csa.size(); ++j) {
+                    printf("%lld ", (uint64_t)cst.csa.text[j]);
+                }
+                printf("\n");
+            }
+        }
+    }
+}
