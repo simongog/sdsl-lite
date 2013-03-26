8e5d0d575fcfe08376385f830f3d605861c7d369
tb38 <timo.beller@uni-ulm.de>
2013-03-26 12:56:20 +0100

    Integrated LACAs again

--- include/sdsl/construct_lcp.hpp
+++ include/sdsl/construct_lcp.hpp
@@ -34,11 +34,8 @@
 
 #include <iostream>
 #include <stdexcept>
-#include <list>
 #include <algorithm>
 #include <fstream>
-#include <queue>
-#include <list>
 
 //#define STUDY_INFORMATIONS
 
@@ -111,7 +108,7 @@ void construct_lcp_kasai(cache_config& config){
  *  \par Space complexity
  *		\f$ n( \log \sigma + \log \n ) \f$ bits 
  *  \pre Text and Suffix array exist in the cache. Keys:
- *         * constants::KEY_TEXT_INT
+ *         * constants::KEY_TEXT for t_width=8  or constants::KEY_TEXT_INT for t_width=0
  *         * constants::KEY_SA 
  *  \post LCP array exist in the cache. Key
  *         * constants::KEY_LCP
@@ -245,96 +242,87 @@ void construct_lcp_bwt_based2(cache_config& config);
 
 
 // semi extern PHI algorithm of Karkainen, Manzini and Puglisi CPM 2009
-//bool construct_lcp_semi_extern_PHI(tMSS& file_map, const std::string& dir, const std::string& id);
+void construct_lcp_semi_extern_PHI(cache_config& config);
 
-//class buffered_char_queue
-//{
-//        typedef bit_vector::size_type size_type;
-//        typedef std::queue<uint8_t> tQ;
-//    private:
-//        static const uint32_t m_buffer_size =  10000;//409600;
-//        uint8_t m_write_buf[m_buffer_size];
-//        uint8_t m_read_buf[m_buffer_size];
-//        size_type 	m_widx; // write index
-//        size_type 	m_ridx; // read index
-//        bool		m_sync; // are read and write buffer the same?
-//        size_type 	m_disk_buffered_blocks; // number of blocks written to disk and not read again yet
-//        char 		m_c;
-//        size_type	m_rb; // read blocks
-//        size_type	m_wb; // written blocks
-//
-//        std::string m_file_name;
-//
-//        std::fstream	m_stream;
-//
-//    public:
-//
-//        buffered_char_queue();
-//        void init(const std::string& dir, char c);
-//        ~buffered_char_queue();
-//        void push_back(uint8_t x);
-//        uint8_t pop_front();
-//};
-//
-//typedef std::list<int_vector<>::size_type> tLI;
-//typedef std::vector<int_vector<>::size_type> tVI;
-//
-//template<class size_type_class>
-//void push_front_m_index(size_type_class i, uint8_t c, tLI(&m_list)[256], uint8_t (&m_chars)[256], size_type_class& m_char_count)
-//{
-//    if (m_list[c].empty()) {
-//        m_chars[m_char_count++] = c;
-//    }
-//    m_list[c].push_front(i);
-//}
-//
-//template<class size_type_class>
-//void push_back_m_index(size_type_class i, uint8_t c, tLI(&m_list)[256], uint8_t (&m_chars)[256], size_type_class& m_char_count)
-//{
-//    if (m_list[c].empty()) {
-//        m_chars[m_char_count++] = c;
-//    }
-//    m_list[c].push_back(i);
-//}
-//
-//// only phase 1 of the new algorithm
-//bool construct_lcp_simple_5n(tMSS& file_map, const std::string& dir, const std::string& id);
-//
+//! Construct the LCP array (only for byte strings)
+/*!	The algorithm computes the lcp array and stores it to disk.
+ *  Only phase 1 of the new algorithm
+ *  \param config	Reference to cache configuration
+ *  \pre Text, Suffix array and BWT exist in the cache. Keys:
+ *         * constants::KEY_TEXT
+ *         * constants::KEY_SA 
+ *         * constants::KEY_BWT 
+ *  \post LCP array exist in the cache. Key
+ *         * constants::KEY_LCP
+ *  \par Time complexity
+ *		  ToDo \f$ \Order{n} \f$
+ *  \par Space complexity
+ *		  ToDo \f$ 5n \f$ bytes
+ *  \par Reference
+ * 	      ToDo
+ */
+void construct_lcp_simple_5n(cache_config& config);
+
 //// only phase 2 of the new algorithm
 //// TODO: assert n > 0
 //bool construct_lcp_simple2_9n(tMSS& file_map, const std::string& dir, const std::string& id);
-//
-////! Our new 2 phases lcp algorithm using usually 2n bytes.
-///*!	The algorithm computes the lcp array and stores it to disk.
-// *  \param file_map A map which contains the filenames of previous computed structures (like suffix array, Burrows and Wheeler transform, inverse suffix array, text,...)
-// *  \param dir		Directory where the lcp array should be stored.
-// *  \param id		Id for the file name of the lcp array.
-// *  \par Space complexity
-// *		Usually \f$ 2n \f$ bytes, worst case \f$5n bytes\f$
-// */
-//bool construct_lcp_go(tMSS& file_map, const std::string& dir, const std::string& id);
-//
-////! Our new 2 phases lcp algorithm using usually 2n bytes.
-///*!	The algorithm computes the lcp array and stores it to disk.
-// *  \param file_map A map which contains the filenames of previous computed structures (like suffix array, Burrows and Wheeler transform, inverse suffix array, text,...)
-// *  \param dir		Directory where the lcp array should be stored.
-// *  \param id		Id for the file name of the lcp array.
-// *  \par Space complexity
-// *		Usually \f$ 2n \f$ bytes, worst case \f$5n bytes\f$
-// */
-//bool construct_lcp_goPHI(tMSS& file_map, const std::string& dir, const std::string& id);
-//
-////! Our new 2 phases lcp algorithm using usually 1 n bytes.
-///*!	The algorithm computes the lcp array and stores it to disk.
-// *  \param file_map A map which contains the filenames of previous computed structures (like suffix array, Burrows and Wheeler transform, inverse suffix array, text,...)
-// *  \param dir		Directory where the lcp array should be stored.
-// *  \param id		Id for the file name of the lcp array.
-// *  \par Space complexity:
-// *		Usually \f$ n+\Order{1} \f$ bytes, worst case \f$ 5n \f$ bytes
-// */
-//bool construct_lcp_go2(tMSS& file_map, const std::string& dir, const std::string& id);
-//
 
+//! Construct the LCP array (only for byte strings)
+/*!	The algorithm computes the lcp array and stores it to disk.
+ *  Our new 2 phases lcp algorithm
+ *  \param config	Reference to cache configuration
+ *  \pre Text, Suffix array and BWT exist in the cache. Keys:
+ *         * constants::KEY_TEXT
+ *         * constants::KEY_SA 
+ *         * constants::KEY_BWT 
+ *  \post LCP array exist in the cache. Key
+ *         * constants::KEY_LCP
+ *  \par Time complexity
+ *		  ToDo \f$ \Order{n} \f$
+ *  \par Space complexity
+ *		  Usually \f$ 2n \f$ bytes, worst case \f$5n bytes\f$
+ *  \par Reference
+ * 	      ToDo
+ */
+void construct_lcp_go(cache_config& config);
+
+//! Construct the LCP array (only for byte strings)
+/*!	The algorithm computes the lcp array and stores it to disk.
+ *  Our new 2 phases lcp algorithm
+ *  \param config	Reference to cache configuration
+ *  \pre Text, Suffix array and BWT exist in the cache. Keys:
+ *         * constants::KEY_TEXT
+ *         * constants::KEY_SA 
+ *         * constants::KEY_BWT 
+ *  \post LCP array exist in the cache. Key
+ *         * constants::KEY_LCP
+ *  \par Time complexity
+ *		  ToDo \f$ \Order{n} \f$
+ *  \par Space complexity
+ *		  Usually \f$ 2n \f$ bytes, worst case \f$5n bytes\f$
+ *  \par Reference
+ * 	      ToDo
+ */
+void construct_lcp_goPHI(cache_config& config);
+
+//! Construct the LCP array (only for byte strings)
+/*!	The algorithm computes the lcp array and stores it to disk.
+ *  Our new 2 phases lcp algorithm using usually 1 n bytes.
+ *  \param config	Reference to cache configuration
+ *  \pre Text, Suffix array and BWT exist in the cache. Keys:
+ *         * constants::KEY_TEXT
+ *         * constants::KEY_SA 
+ *         * constants::KEY_BWT 
+ *  \post LCP array exist in the cache. Key
+ *         * constants::KEY_LCP
+ *  \par Time complexity
+ *		  ToDo \f$ \Order{n} \f$
+ *  \par Space complexity
+ *		 Usually \f$ n+\Order{1} \f$ bytes, worst case \f$ 5n \f$ bytes
+ *  \par Reference
+ * 	      ToDo
+ */
+void construct_lcp_go2(cache_config& config);
 
 
 void lcp_info(tMSS& file_map);
