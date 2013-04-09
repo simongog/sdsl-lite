2d29ab3d275e056d65f115c648f97e6bf0623663
Simon Gog <simon.gog@gmail.com>
2013-04-09 17:10:37 +1000

    printf arguments fixed.

--- examples/read_ints.cpp
+++ examples/read_ints.cpp
@@ -10,7 +10,6 @@ int main()
 {
     {
         typedef csa_wt<wt_int<>, 32, 64, sa_order_sa_sampling<>, int_vector<>, int_alphabet_strategy<> > t_csa;
-//		typedef csa_wt<> t_csa;
         typedef cst_sct3<t_csa> t_cst;
         t_cst cst;
         std::streamsize BUF_SIZE=4096;
@@ -19,9 +18,9 @@ int main()
             construct_im(cst, (const char*)text, 'd');
             printf("   i  SA LCP     T\n");
             for (uint64_t i=0; i <cst.csa.size(); ++i) {
-                printf("%4lld %3lld %3lld     ", i, cst.csa[i], cst.lcp[i]);
+                printf("%4llu %3llu %3llu     ", i, cst.csa[i], cst.lcp[i]);
                 for (uint64_t j=cst.csa[i]; j < cst.csa.size(); ++j) {
-                    printf("%lld ", (uint64_t)cst.csa.text[j]);
+                    printf("%llu ", (uint64_t)cst.csa.text[j]);
                 }
                 printf("\n");
             }
