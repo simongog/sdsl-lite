0192ce2be5b45c1e7543dd5e891ef8ace3e0b674
Simon Gog <simon.gog@gmail.com>
2013-04-18 13:10:20 +1000

    Now you can call the script from any directory.

--- extras/mm-log-visualize/mm-log.sh
+++ extras/mm-log-visualize/mm-log.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-CUR_DIR=`pwd`
-OLD_DIR="$( cd "$( dirname "$0" )" && pwd )" # gets the directory where the script is located in
-cd "${OLD_DIR}"
-
 if [ $# -lt 1 ]; then
 	echo "Usage: ${0} input tikz=0 divSpace=1 Time=seconds ylabelAdd=\" (in bytes))\""
 	echo " input   : file where the output of mm-log.x is stored"
 	echo " tikz = 0: output in mm-log.pdf"
 	echo " tikz = 1: output in mm-log.tex"
 	echo " divSpace: divide space by this number"
-	echo " divTime : seconds|milliseconds"
+	echo " divTime : seconds | milliseconds"
 	exit
 fi
+
+CUR_DIR=`pwd`
+
+cd `dirname $1`
+INPUT_DIR=`pwd`
+INPUT=${INPUT_DIR}/`basename $1`
+
+echo "INPUT=${INPUT}"
+cd ${CUR_DIR}
+
+OLD_DIR="$( cd "$( dirname "$0" )" && pwd )" # gets the directory where the script is located in
+cd "${OLD_DIR}"
 
 TIKZ="0"
 if [ $# -gt 1 ]; then
@@ -31,6 +39,6 @@ if [ $# -gt 4 ]; then
 	YLABELADD=$5
 fi
 
-R --vanilla --args "../../benchmark/basic_functions.R" "$1" "${TIKZ}" "${DIVSPACE}" "${TIME}" "${YLABELADD}" < mm-log.R
-
-
+MM_DIR=`pwd`
+echo "MM_DIR=${MM_DIR}"
+R --vanilla --args "${MM_DIR}" "${INPUT}" "${TIKZ}" "${DIVSPACE}" "${TIME}" "${YLABELADD}" < mm-log.R
