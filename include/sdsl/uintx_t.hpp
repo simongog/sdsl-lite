310d305260fdc426959d0e7c4859ff86eff8ce9c
Simon Gog <simon.gog@gmail.com>
2013-06-13 23:32:19 +1000

    Use cstdint header now for fixed width int types.

---
+++ include/sdsl/uintx_t.hpp
@@ -0,0 +1,16 @@
+#ifndef INCLUDED_SDSL_UINTX_T
+#define INCLUDED_SDSL_UINTX_T
+
+#include <cstdint>
+
+using std::int8_t;
+using std::int16_t;
+using std::int32_t;
+using std::int64_t;
+
+using std::uint8_t;
+using std::uint16_t;
+using std::uint32_t;
+using std::uint64_t;
+
+#endif
