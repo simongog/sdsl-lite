c5b48fa7a7c080a6d25e3d1b63ad1f562e161b4e
Timo Beller <timo.beller@uni-ulm.de>
2013-07-12 08:30:33 +0200

    Addex copyright

--- include/sdsl/int_vector_buffer.hpp
+++ include/sdsl/int_vector_buffer.hpp
@@ -1,3 +1,23 @@
+/* sdsl - succinct data structures library
+    Copyright (C) 2008-2013 Simon Gog
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/ .
+*/
+/*! \file int_vector_buffer.hpp
+    \brief int_vector_buffer.hpp contains the sdsl::int_vector_buffer class.
+    \author Maike Zwerger, Timo Beller and Simon Gog
+*/
 #ifndef INCLUDED_INT_VECTOR_BUFFER
 #define INCLUDED_INT_VECTOR_BUFFER
 
