85c37e6f803e9a406478c757e9234ffd25ee5513
Simon Gog <simon.gog@gmail.com>
2013-08-27 14:08:40 +1000

    Added example for Leonardo

---
+++ tutorial/use-a-wavelet-tree.cpp
@@ -0,0 +1,27 @@
+#include <sdsl/wavelet_trees.hpp>
+#include <iostream>
+
+using namespace sdsl;
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2) {
+        cout << "Usage: " << argv[1] << " file" << endl;
+        return 1;
+    }
+    wt_huff<> wt;
+    construct(wt, argv[1], 1);
+
+    cout << "wt.size()="<< wt.size() << endl;
+    cout << "wt.sigma ="<< wt.sigma << endl;
+    if (wt.size() > 0) {
+        // access an element
+        cout << "wt[0]=" << wt[0] << endl;
+        // rank an element (exclude)
+        uint64_t r = wt.rank(wt.size(), wt[0]);
+        cout << "wt.rank(wt.size(), wt[0])=" << r  << endl;
+        // select element ()
+        cout << "wt.select(r, wt[0]) = " << wt.select(r, wt[0]) << endl;
+    }
+}
