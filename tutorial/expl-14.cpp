0908d53120abea51f7210dd986a79bc2645ffa18
Simon Gog <simon.gog@gmail.com>
2013-09-13 12:17:48 +1000

    Adjusted example.

--- tutorial/expl-14.cpp
+++ tutorial/expl-14.cpp
@@ -6,14 +6,13 @@ using namespace sdsl;
 
 int main()
 {
-    wt_blcd<rrr_vector<63>> wt;
+    wt_hutu<rrr_vector<63>> wt;
     construct_im(wt, "ハローワールド！", 1);
     for (size_t i=0; i < wt.size(); ++i)
         cout << wt[i];
     cout << endl;
-    wt_hutu<>::size_type cnt1 = 0, cnt2 = 0, _;
-    wt.lex_count(0, wt.size(), 0x80, cnt1, _);
-    wt.lex_count(0, wt.size(), 0xbf, _, cnt2);
-    cout << "# of chars        : " << wt.size() << endl;
-    cout << "# of UTF-8 symbols: " << cnt1 + cnt2 << endl;
+    auto t1 = wt.lex_count(0, wt.size(), 0x80);
+    auto t2 = wt.lex_count(0, wt.size(), 0xbf);
+    cout << "# of bytes        : " << wt.size() << endl;
+    cout << "# of UTF-8 symbols: " << get<1>(t1) + get<2>(t2) << endl;
 }
