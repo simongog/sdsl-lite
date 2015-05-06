b9befd5f983b9e0f22f9bdbb333a2d6b657d1489
Johannes Bader <jogus@live.de>
2015-05-06 12:56:38 +0200

    Ignore personal _scratch folder

--- _scratch/build.sh
+++
@@ -1,5 +0,0 @@
-echo
-echo -e '\033[1m REBUILD \033[0m'
-echo
-g++ -std=c++11 -O3 -ffast-math -funroll-loops -DNDEBUG -I ../include -I ../build/external/libdivsufsort-2.0.1/include -L ~/lib scratch.cpp -o scratch -lsdsl -ldivsufsort -ldivsufsort64
-./scratch input.txt
