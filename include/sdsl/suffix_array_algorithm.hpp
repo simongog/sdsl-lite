caa2f69dd56ef20e641512cfa5fde0efe33ef063
Johannes Bader <jogus@live.de>
2015-05-06 12:39:41 +0200

    Forward search initial commit

--- include/sdsl/suffix_array_algorithm.hpp
+++ include/sdsl/suffix_array_algorithm.hpp
@@ -27,6 +27,108 @@
 namespace sdsl
 {
 
+//! Compare a pattern with a CSA-prefix (truncated to length $|pattern|$).
+/*!
+ * \tparam t_csa      A CSA type.
+ * \tparam t_pat_iter Pattern iterator type.
+ *
+ * \param csa   The CSA object.
+ * \param begin Iterator to the begin of the pattern (inclusive).
+ * \param end   Iterator to the end of the pattern (exclusive).
+ * \param i     Index of CSA-prefix.
+ * \return Equals one, if the pattern is lexicographically larger than the prefix.
+ *         Equals minus one, if the pattern is lexicographically smaller than the prefix.
+ *            Equals zero, if the pattern equals the (truncated) prefix.
+ *
+ */
+template<class t_csa, class t_pat_iter>
+int compare(
+    const t_csa& csa,
+    t_pat_iter begin,
+    t_pat_iter end,
+    typename t_csa::size_type i
+)
+{
+    for (; begin != end; begin++)
+    {
+        typename t_csa::size_type index = csa.char2comp[*begin];
+        if (index == 0) return -1;
+        if (csa.C[index + 1] - 1 < i) return -1;
+        if (csa.C[index] > i) return 1;
+        i = csa.psi[i];
+    }
+    return 0;
+}
+
+//! Forward search for a pattern in an \f$\omega\f$-interval \f$[\ell..r]\f$ in the CSA.
+/*!
+ * \tparam t_csa      A CSA type.
+ * \tparam t_pat_iter Pattern iterator type.
+ *
+ * \param csa   The CSA object.
+ * \param l     Left border of the lcp-interval \f$ [\ell..r]\f$.
+ * \param r     Right border of the lcp-interval \f$ [\ell..r]\f$.
+ * \param begin Iterator to the begin of the pattern (inclusive).
+ * \param end   Iterator to the end of the pattern (exclusive).
+ * \param l_res New left border.
+ * \param r_res New right border.
+ * \return The size of the new interval [\ell_{new}..r_{new}].
+ *         Equals zero, if no match is found.
+ *
+ */
+template<class t_csa, class t_pat_iter>
+typename t_csa::size_type
+forward_search(
+    const t_csa& csa,
+    typename t_csa::size_type l,
+    typename t_csa::size_type r,
+    t_pat_iter begin,
+    t_pat_iter end,
+    typename t_csa::size_type& l_res,
+    typename t_csa::size_type& r_res,
+    SDSL_UNUSED typename std::enable_if<std::is_same<csa_tag, typename t_csa::index_category>::value, csa_tag>::type x = csa_tag()
+)
+{
+    typename t_csa::size_type size = csa.size();
+
+    l_res = 0;
+    r_res = -1;
+    typename t_csa::size_type l_res_upper = size;
+    typename t_csa::size_type r_res_upper = size;
+
+    // shortcut for too long patterns... worth it?
+    //if ((typename t_csa::size_type)(end - begin) > size)
+    //    return 0;
+
+    // binary search (on min)
+    while (l_res < l_res_upper)
+    {
+        typename t_csa::size_type sample = (l_res + l_res_upper) / 2;
+        int result = compare(csa, begin, end, sample);
+        if (result == 1)
+            l_res = sample + 1;
+        else if (result == -1)
+            l_res_upper = sample;
+        else
+            l_res_upper = sample;
+    }
+
+    // binary search (on max)
+    while (r_res + 1 < r_res_upper)
+    {
+        typename t_csa::size_type sample = (r_res + r_res_upper) / 2;
+        int result = compare(csa, begin, end, sample);
+        if (result == 1)
+            r_res = sample;
+        else if (result == -1)
+            r_res_upper = sample;
+        else
+            r_res = sample;
+    }
+
+    return r_res - l_res + 1;
+}
+
 //! Backward search for a character c in an \f$\omega\f$-interval \f$[\ell..r]\f$ in the CSA.
 /*!
  * \tparam t_csa CSA type.
