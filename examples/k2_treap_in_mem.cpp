4a5b2c69c4dc4e4c1482387f56318425772f6b56
Jan Broß <jan.bross@sap.com>
2016-06-21 16:50:34 +0200

    Add loader for webgraph format

--- examples/k2_treap_in_mem.cpp
+++ examples/k2_treap_in_mem.cpp
@@ -15,38 +15,29 @@ int main()
     k2_rrr k2treap;
 
     // Initialize treap with a vector of (x,y,weight) elements
-    construct_im(k2treap, {{1,2,3},{2,2,6},{4,4,1},{3,3,2},{3,1,8}});
+    construct_im(k2treap, {{1,2},{2,2},{4,4},{3,3},{3,1}});
 
     cout << "Points in the k2treap: " << k2treap.size() << endl;
 
     cout << "Points in the rectangle from (2,1) to (3,3): " ;
     cout << count(k2treap, {2,1}, {3,3}) << endl;
 
-    cout << "Heaviest points in rectangle from (0,0) to (2,8):" << endl;
-    auto topk_it = top_k(k2treap, {0,0}, {2,8});
-    while (topk_it) {
-        auto point_weight = *topk_it;
-        cout << point_weight.first <<" weight: "<<point_weight.second << endl;
-        ++topk_it;
-    }
-
     cout << "Report all points in rectangle from (2,2) to (10,10)" << endl;
-    cout << "with weight in [2..6]:" << endl;
-    auto range_it = range_3d(k2treap, {2,2}, {10,10}, {2,100});
+    auto range_it = range_3d(k2treap, {2,2}, {10,10});
     while (range_it) {
-        auto point_weight = *range_it;
-        cout << point_weight.first <<" weight: "<<point_weight.second << endl;
+        auto p = *range_it;
+        cout << real(p) <<" weight: "<< imag(p) << endl;
         ++range_it;
     }
 
     cout<<"---"<<endl;
     {
         k2_rrr k2t;
-        construct_im(k2t, {{1,2,3},{2,3,3},{3,1,3}});
-        auto topk_it = top_k(k2t, {0,0}, {10,10});
+        construct_im(k2t, {{1,2},{2,3},{3,1}});
+        auto topk_it = range_3d(k2t, {0,0}, {10,10});
         while (topk_it) {
-            auto point_weight = *topk_it;
-            cout << point_weight.first <<" weight: "<<point_weight.second << endl;
+            auto p = *topk_it;
+            cout << real(p) <<" weight: "<< imag(p) << endl;
             ++topk_it;
         }
     }
---
+++ examples/load_k2_treap_from_webgraph_txt.hpp.cpp
@@ -0,0 +1,173 @@
+//
+// Created by d056848 on 6/7/16.
+//
+
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <string>
+#include <complex>
+#include <sdsl/k2_treap.hpp>
+#include <sdsl/bit_vectors.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/tokenizer.hpp>
+#include <sys/times.h>
+
+using std::ifstream;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+using ::std::ofstream;
+using namespace sdsl;
+using namespace std;
+using namespace boost;
+
+/* Time meassuring */
+double ticks;
+struct tms t1, t2;
+
+void start_clock() {
+    times(&t1);
+}
+
+double stop_clock() {
+    times(&t2);
+    return (t2.tms_utime - t1.tms_utime) / ticks;
+}
+
+int main(int argc, char *argv[]) {
+
+
+/* end Time meassuring */
+
+    /*Graph Format:
+    <NumNodes>
+    Source  Target
+    Source  Target
+
+
+    First lines of eu2005
+     862664
+     0       240
+     0       420
+     0       620
+     0       630
+    */
+    if (argc < 3) {
+        fprintf(stderr, "USAGE: %s <GRAPH> <Output-File>\n <GRAPH> has to be in the .graph-txt format", argv[0]);
+        return (-1);
+    }
+
+    std::string fileName(argv[1]);
+    fileName.append(".graph-txt");
+    std::fstream fileStream(fileName, std::ios_base::in);
+
+    //simple string dictionary
+    if (fileStream.is_open()) {
+        cout << "Reading file " << argv[1] << endl;
+        //std::vector<std::tuple<unsigned int, unsigned int>> idBuffer;
+
+        std::string readBuffer;
+        //read lines
+
+        std::getline(fileStream, readBuffer);
+        uint numberOfNodes = stoul(readBuffer);
+        vector <pair<uint64_t, uint64_t>> coordinates;
+
+        while (std::getline(fileStream, readBuffer)) {
+            //tokenizer<escaped_list_separator<char> > tok(readBuffer);
+            vector <string> sourceNodeAndTargets;
+            boost::split(sourceNodeAndTargets, readBuffer, boost::is_any_of("\t"));
+
+            uint sourceId = std::stoul(sourceNodeAndTargets[0].c_str());
+            uint targetId = std::stoul(sourceNodeAndTargets[1].c_str()) / 10;//Strange 0 delimited format
+            //cout << "adding " << sourceId << "," << targetId << endl;
+            coordinates.push_back(make_pair(sourceId, targetId));
+        }
+
+        fileStream.close();
+
+        std::cerr << "Finished Reading File \n" << std::endl;
+
+        typedef k2_treap<3, rrr_vector<63>> k2_rrr;
+        k2_rrr k2treap;
+
+
+        double t2 = 0;
+        ticks = (double) sysconf(_SC_CLK_TCK);
+        start_clock();
+        // Initialize treap with a vector of (x,y,weight) elements
+        construct_im(k2treap, coordinates);
+
+        t2 += stop_clock();
+        t2 *= 1000; // to milliseconds
+
+        fprintf(stderr, "Initialization time (ms): %f\n", t2);
+
+        std::cerr << "Processing direct neighbour queries\n" << std::endl;
+
+        uint i;
+
+        srand(0);
+        std::vector<uint> queries;
+        uint queryCount = 10000;
+        for (i = 0; i < queryCount; i++) {
+            queries.push_back(rand() % 862664);
+        }
+
+
+        double t = 0;
+        start_clock();
+        uint count = 0;
+        for (auto query : queries){
+            auto range_it = range_3d(k2treap, {query,0}, {query,UINT_MAX});
+            while (range_it) {
+                ++range_it;
+                count++;
+            }
+        }
+
+        t += stop_clock();
+        t *= 1000; // to milliseconds
+
+        fprintf(stderr, "Total time(ms): %f\n", t);
+        fprintf(stderr,"Recovered Nodes: %lu\n",count);
+        fprintf(stderr,"Queries: %d\n",queryCount);
+        fprintf(stderr,"Total time(ms): %f",t);
+        fprintf(stderr,"Time per query: %f\n",t/queryCount);
+        fprintf(stderr,"Time per link: %f\n",t/count);
+
+
+
+        std::cerr << "Processing reverse neighbour queries\n" << std::endl;
+
+        double t3 = 0;
+        start_clock();
+        uint count2 = 0;
+        for (auto query : queries){
+            auto range_it = range_3d(k2treap, {0,query}, {UINT_MAX,query});
+            while (range_it) {
+                ++range_it;
+                count2++;
+            }
+        }
+
+        //uint count = 342045;
+        t3 += stop_clock();
+        t3 *= 1000; // to milliseconds
+
+        fprintf(stderr, "Total time(ms): %f\n", t3);
+        fprintf(stderr,"Recovered Nodes: %lu\n",count2);
+        fprintf(stderr,"Queries: %d\n",queryCount);
+        fprintf(stderr,"Total time(ms): %f",t3);
+        fprintf(stderr,"Time per query: %f\n",t3/queryCount);
+        fprintf(stderr,"Time per link: %f\n",t3/count2);
+
+
+        store_to_file(k2treap,argv[2]);
+        write_structure<HTML_FORMAT>(k2treap,"structure.json");
+    } else {
+        throw "Could not load file";
+    }
+}
