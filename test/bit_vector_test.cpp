5e5b04802c0bd14be50145af5af4798c7d9d4ca6
Simon Gog <simon.gog@gmail.com>
2016-06-29 15:09:17 +0200

    sd_vector_builder now works for all sd_vector configuration
    sd_vector_builder was just working for sd_vector<bit_vector,....>.
    This is fix now. Also adjusted tests.

--- test/bit_vector_test.cpp
+++ test/bit_vector_test.cpp
@@ -29,7 +29,7 @@ rrr_vector<192>,
 rrr_vector<255>,
 rrr_vector<15>,
 rrr_vector<31>,
-rrr_vector<63>,
+rrr_vector
 rrr_vector<83>,
 rrr_vector<127>,
 rrr_vector<128>,
@@ -108,27 +108,6 @@ TYPED_TEST(bit_vector_test, swap)
         ASSERT_EQ((bool)(bv[j]), (bool)(bv_empty[j]));
     }
 }
-
-/*
-TEST(SD_VECTOR, IteratorConstructor)
-{
-    std::vector<uint64_t> pos;
-    bit_vector bv(100000);
-    std::mt19937_64 rng;
-    std::uniform_int_distribution<uint64_t> distribution(0, 9);
-    auto dice = bind(distribution, rng);
-    for (size_t i=0; i < bv.size(); ++i) {
-        if (0 == dice()) {
-            pos.emplace_back(i);
-            bv[i] = 1;
-        }
-    }
-    sd_vector<> sdv(pos.begin(),pos.end());
-    for (size_t i=0; i < bv.size(); ++i) {
-        ASSERT_EQ((bool)sdv[i],(bool)bv[i]);
-    }
-}
-*/
 
 }// end namespace
 
