3af74dc163b4a3aade5fd73dfe5861c72e255166
Jan Broß <jan.bross@sap.com>
2016-09-26 10:48:35 +0200

    Add template functions for construct and construct_counting sort to reduce code bloat

--- include/sdsl/k2_tree_base.hpp
+++ include/sdsl/k2_tree_base.hpp
@@ -1236,6 +1236,273 @@ namespace sdsl {
             }
         }
 
+        /**
+        * Constructs the tree corresponding to the points in the links vector by using counting
+        * sort with k² buckets and rearranging the input on every level of the tree
+        *
+        * Uses template functions for faster /k^l and k^l operations, virtual methods providing the functionality would be
+        * a cleaner, but much slower way
+        *
+        * @param links
+        * @param temp_file_prefix
+        */
+        template<typename t_vector, typename Fun, typename Fun2>
+        void construct_counting_sort_internal(t_vector &links, Fun divexp, Fun2 exp, std::string temp_file_prefix = "") {
+            using namespace k2_treap_ns;
+            typedef decltype(links[0].first) t_x;
+            typedef decltype(links[0].second) t_y;
+            using t_e = std::pair<t_x, t_y>;
+
+            this->m_size = links.size();
+
+            if (this->m_size == 0) {
+                return;
+            }
+
+            //std::cout << "Using counting sort" << std::endl;
+
+            std::string id_part = util::to_string(util::pid())
+                                  + "_" + util::to_string(util::id());
+
+            {
+                //                  upper left          lower right                 interval in links   level
+                typedef std::tuple<std::pair<t_x, t_y>, std::pair<uint64_t, uint64_t>, t_e, uint8_t> t_queue;
+                std::queue<t_queue> queue;
+
+                //partition recursively until reaching the leaves
+                queue.push(t_queue(std::make_pair<t_x, t_y>(0, 0),
+                                   std::make_pair(this->m_max_element - 1, this->m_max_element - 1),
+                                   t_e(0, links.size()), 0));
+
+                uint64_t number_of_bits = 0; //for speed comparison purposes of different k
+                uint8_t previous_level = 0;
+                uint64_t ctr = 0;
+                uint64_t set_bits_in_level = 0;
+                bit_vector buffer(get_k(0) * get_k(0));
+                this->m_levels.resize(this->m_tree_height - 1);
+                while (!queue.empty()) {
+                    auto upper_left = std::get<0>(queue.front());
+                    auto lower_right = std::get<1>(queue.front());
+                    auto links_interval = std::get<2>(queue.front());
+                    auto current_level = std::get<3>(queue.front());
+                    auto inv_level = this->m_tree_height - current_level - 1; //inverse level for accessing precomps
+
+                    const uint8_t k = get_k(current_level);
+                    if (current_level > previous_level) {
+                        previous_level = current_level;
+                        this->m_levels[current_level - 1] = buffer;
+                        bit_vector tmp(set_bits_in_level * k * k);
+                        tmp.swap(buffer);
+                        ctr = 0;
+                        set_bits_in_level = 0;
+                    }
+
+                    std::vector<t_x> intervals(k * k + 1);
+
+                    queue.pop();
+
+                    //do counting sort
+                    auto x1 = upper_left.first;
+                    auto y1 = upper_left.second;
+                    for (uint64_t j = links_interval.first; j < links_interval.second; ++j) {
+                        auto x = links[j].first;
+                        auto y = links[j].second;
+                        auto p1 = divexp(x - x1, inv_level);
+                        auto p2 = divexp(y - y1, inv_level);
+
+                        //FIXME: replace by bitshift for k
+                        auto corresponding_matrix = p1 * k + p2;
+                        intervals[corresponding_matrix +
+                                  1]++;//offset corresponding matrix by one to allow for more efficient in interval comparision
+                    }
+
+                    intervals[0] = 0;
+
+                    //append bits to level_vectors[level] based on result
+                    for (uint i = 1; i < intervals.size(); ++i) {
+//                        item_count[current_level].push_back(intervals[i])
+//                        utilization[current_level].push_back(
+//                                ((double) intervals[i]) / (submatrix_size * submatrix_size) * 100);
+
+                        if (intervals[i] > 0) {
+                            buffer[ctr] = 1;
+                            //std::cout << "1";
+                            set_bits_in_level++;
+                        } /*else {
+                            buffer[ctr] = 0;
+                        }*/
+                        number_of_bits++;
+                        ctr++;
+                        intervals[i] += intervals[i - 1]; //build prefix sum
+                    }
+
+                    //leaves not reached yet --> append to level_vector & reorder
+                    if (inv_level > 0) {
+                        std::vector<t_x> offset(k * k);
+                        offset[0] = 0;
+                        for (size_t l = 1; l < offset.size(); ++l) {
+                            offset[l] = intervals[l] + 1;
+                        }
+
+                        auto begin = links.begin() + links_interval.first;
+                        auto it = begin;
+
+                        //reorder links based on counting sort offsets
+                        uint64_t index = 0;
+                        while (it != links.begin() + links_interval.second) {
+                            uint corresponding_matrix =
+                                    divexp(((*it).first - x1), inv_level) * k +
+                                    divexp((*it).second - y1, inv_level);
+
+                            if (index >= intervals[corresponding_matrix] &&
+                                index < intervals[corresponding_matrix + 1]) {
+                                //element is at correct position
+                                offset[corresponding_matrix]++;
+
+                                it++;
+                                index++;
+                            } else {
+                                //swap to correct position either swapping a match or a non match back
+                                //there are at most m swaps if all m elements are at the wrong position
+                                //every value is at most read twice
+                                std::iter_swap(it, begin + offset[corresponding_matrix] - 1);
+                                offset[corresponding_matrix]++;
+                            }
+                        }
+
+                        //enqueue submatrixes
+                        auto new_submatrix_size = exp(inv_level);
+                        for (uint x = 0; x < k; ++x) {
+                            for (uint y = 0; y < k; ++y) {
+                                auto new_interval = std::make_pair(intervals[x * k + y] + links_interval.first,
+                                                                   intervals[x * k + y + 1] + links_interval.first);
+                                if (new_interval.first != new_interval.second) {
+                                    auto new_upper_left = std::make_pair<t_x, t_y>(
+                                            x * new_submatrix_size + upper_left.first,
+                                            y * new_submatrix_size + upper_left.second);
+                                    auto new_lower_right = std::make_pair<t_x, t_y>(
+                                            (x + 1) * new_submatrix_size - 1 + upper_left.first,
+                                            (y + 1) * new_submatrix_size - 1 + upper_left.second);
+                                    queue.push(std::make_tuple(new_upper_left, new_lower_right, new_interval,
+                                                               current_level + 1));
+                                }
+                            }
+                        }
+                    }
+                }
+
+                this->m_leaves = t_leaf(buffer);
+
+                this->m_levels_rank.resize(this->m_levels.size());
+                for (uint64_t i = 0; i < this->m_levels.size(); ++i) {
+                    util::init_support(this->m_levels_rank[i], &this->m_levels[i]);
+                }
+            }
+        }
+
+        /**
+        * Constructs the tree corresponding to the points in the links vector by partitioning the input multiple times
+        *
+        * Uses template functions for faster /k^l and k^l operations, virtual methods providing the functionality would be
+        * a cleaner, but much slower way
+        *
+        * @param links
+        * @param temp_file_prefix
+        */
+        template<typename t_vector, typename Fun>
+        void construct_internal(t_vector &links, Fun divexp, std::string temp_file_prefix = "") {
+            using namespace k2_treap_ns;
+            typedef decltype(links[0].first) t_x;
+            typedef decltype(links[0].second) t_y;
+            using t_e = std::pair<t_x, t_y>;
+
+            this->m_size = links.size();
+
+            if (this->m_tree_height == 0) {//might occur in k2part
+                return;
+            }
+
+            std::string id_part = util::to_string(util::pid())
+                                  + "_" + util::to_string(util::id());
+
+            {
+                std::vector<int_vector_buffer<1>> level_buffers = this->create_level_buffers(temp_file_prefix, id_part);
+
+                auto end = std::end(links);
+                //uint64_t last_level_bits = 0;
+                //uint64_t level_bits = 0;
+
+                //recursively partition that stuff
+                uint64_t submatrix_size = this->m_max_element;
+                for (int l = this->m_tree_height; l + 1 > 0; --l) {
+
+                    //std::cout << "Processing Level " << l << std::endl;
+                    //level_bits = 0;
+
+                    uint8_t k = 0;
+                    uint64_t current_submatrix_size = 0;
+                    if (l > 0) {
+                        k = get_k(this->m_tree_height - l);
+                        current_submatrix_size = submatrix_size / k;
+                    }
+
+                    auto sp = std::begin(links);
+                    for (auto ep = sp; ep != end;) {
+
+                        //Iterator which only returns the nodes within a certain subtree
+                        ep = std::find_if(sp, end, [=, &sp, &l](const t_e &e) {
+                            auto x1 = std::get<0>(*sp);
+                            auto y1 = std::get<1>(*sp);
+                            auto x2 = std::get<0>(e);
+                            auto y2 = std::get<1>(e);
+                            bool in_sub_tree = divexp(x1, l) != divexp(x2, l)
+                                               or divexp(y1, l) != divexp(y2, l);
+
+                            return in_sub_tree;
+                        });
+
+
+                        if (l > 0) {
+                            auto _sp = sp;
+
+                            for (uint8_t i = 0; i < k; ++i) {
+                                auto _ep = ep;
+                                if (i + 1 < k) {  //partition t_k -1 times vertically (1 in the case of k=2)
+                                    _ep = std::partition(_sp, _ep, [=, &i, &l](const t_e &e) {
+                                        return (divexp(std::get<0>(e), l - 1) & (k-1))<= i;
+                                    });
+                                }
+                                auto __sp = _sp;
+
+                                for (uint8_t j = 0;
+                                     j < k; ++j) { //partition the t_k vertical partitions t_k -1 times horizontally
+                                    auto __ep = _ep;
+                                    if (j + 1 < k) {
+                                        __ep = std::partition(__sp, _ep, [=, &j, &l](const t_e &e) {
+                                            return (divexp(std::get<1>(e), l - 1) & (k-1)) <= j;
+                                        });
+                                    }
+                                    bool not_empty = __ep > __sp;
+                                    level_buffers[this->m_tree_height - l].push_back(not_empty);
+                                    //level_bits++;
+                                    __sp = __ep;
+                                }
+                                _sp = _ep;
+                            }
+                        }
+                        if (ep != end) {
+                            //++ep;
+                            sp = ep;
+                        }
+                    }
+                    //last_level_bits = level_bits;
+                    submatrix_size = current_submatrix_size;
+                }
+            }
+            this->load_vectors_from_file(temp_file_prefix, id_part);
+        }
+
+
     private:
 
         /*##################### Leaf Access for legacy dac compressed version #######################################**/
