200d4c8b66249d8b1712391496ed27e6e47a7c80
Kevin Murray <spam@kdmurray.id.au>
2016-11-08 11:08:30 +1100

    Correct paths in pkg-config template

--- sdsl-lite.pc.cmake
+++ sdsl-lite.pc.cmake
@@ -1,7 +1,7 @@
 prefix=@CMAKE_INSTALL_PREFIX@
 exec_prefix=${prefix}
-libdir=@CMAKE_INSTALL_LIBDIR@
-includedir=@CMAKE_INSTALL_INCLUDEDIR@
+libdir=${prefix}/lib
+includedir=${prefix}/include
 
 Name: @PROJECT_NAME@
 Description: @PROJECT_DESCRIPTION@
