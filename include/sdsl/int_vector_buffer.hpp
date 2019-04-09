0b4181d15d8b815155c37c8218b5e4ae43f611b4
Christopher Barber <christopher.barber@inf.ethz.ch>
2019-04-09 11:43:55 +0200

    support file offset in int_vector_buffer
    open int_vector_buffer file for out if out|in fails (fix failing test)
    
    bugfix: buffer assignment and swap

--- include/sdsl/int_vector_buffer.hpp
+++ include/sdsl/int_vector_buffer.hpp
@@ -44,6 +44,7 @@ class int_vector_buffer
         static_assert(t_width <= 64 , "int_vector_buffer: width must be at most 64 bits.");
         sdsl::isfstream     m_ifile;
         sdsl::osfstream     m_ofile;
+        uint64_t            m_start;
         std::string         m_filename;
         int_vector<t_width> m_buffer;
         bool                m_need_to_write = false;
@@ -139,7 +140,7 @@ class int_vector_buffer
          *                    If true the file will be interpreted as plain array with t_width bits per integer.
          *                    In second case (is_plain==true), t_width must be 8, 16, 32 or 64.
          */
-        int_vector_buffer(const std::string filename, std::ios::openmode mode=std::ios::in, const uint64_t buffer_size=1024*1024, const uint8_t int_width=t_width, const bool is_plain=false)
+        int_vector_buffer(const std::string filename, std::ios::openmode mode=std::ios::in, const uint64_t buffer_size=1024*1024, const uint8_t int_width=t_width, const bool is_plain=false, const uint64_t file_offset=0)
         {
             m_filename = filename;
             assert(!(mode&std::ios::app));
@@ -153,10 +154,15 @@ class int_vector_buffer
             }
 
             // Open file for IO
-            m_ofile.open(m_filename, mode|std::ios::out|std::ios::binary);
+            assert(file_offset>=0);
+            m_start = file_offset;
+            m_ofile.open(m_filename, mode|std::ios::out|(m_start ? std::ios::in : std::ios::openmode(0))|std::ios::binary);
             assert(m_ofile.good());
+            m_ofile.seekp(m_start);
             m_ifile.open(m_filename, std::ios::in|std::ios::binary);
             assert(m_ifile.good());
+            m_ifile.seekg(m_start);
+            m_offset += m_start;
             if (mode & std::ios::in) {
                 uint64_t size  = 0;
                 if (is_plain) {
@@ -178,6 +184,7 @@ class int_vector_buffer
             m_filename(std::move(ivb.m_filename)),
             m_buffer(std::move(ivb.m_buffer)),
             m_need_to_write(ivb.m_need_to_write),
+            m_start(ivb.m_start),
             m_offset(ivb.m_offset),
             m_buffersize(ivb.m_buffersize),
             m_size(ivb.m_size),
@@ -219,6 +226,7 @@ class int_vector_buffer
             // assign the values of ivb to this
             m_buffer = static_cast<int_vector<t_width>&&>(ivb.m_buffer);
             m_need_to_write = ivb.m_need_to_write;
+            m_start = ivb.m_start;
             m_offset = ivb.m_offset;
             m_buffersize = ivb.m_buffersize;
             m_size = ivb.m_size;
@@ -334,9 +342,9 @@ class int_vector_buffer
             if (is_open()) {
                 if (!remove_file) {
                     write_block();
-                    if (0 < m_offset) { // in case of int_vector, write header and trailing zeros
+                    if (m_start < m_offset) { // in case of int_vector, write header and trailing zeros
                         uint64_t size = m_size*width();
-                        m_ofile.seekp(0, std::ios::beg);
+                        m_ofile.seekp(m_start, std::ios::beg);
                         int_vector<t_width>::write_header(size, width(), m_ofile);
                         assert(m_ofile.good());
                         uint64_t wb = (size+7)/8;
@@ -387,6 +395,7 @@ class int_vector_buffer
                 assert(ivb.m_ofile.good());
                 std::swap(m_buffer, ivb.m_buffer);
                 std::swap(m_need_to_write, ivb.m_need_to_write);
+                std::swap(m_start, ivb.m_start);
                 std::swap(m_offset, ivb.m_offset);
                 std::swap(m_buffersize, ivb.m_buffersize);
                 std::swap(m_size, ivb.m_size);
