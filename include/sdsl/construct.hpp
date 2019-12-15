simongog/sdsl-lite

commit cbcbbcbb9dc1f1a0aa8b89de67fe9e8f1bedc1de

a bug when loading int_vector to buffer fixed, redundant copying removed

include/sdsl/construct.hpp
@@ -53,7 +53,7 @@ void append_zero_symbol(int_vector& text)
 
 
 template<class t_index>
-void construct(t_index& idx, std::string file, uint8_t num_bytes=0)
+void construct(t_index& idx, const std::string &file, uint8_t num_bytes=0)
 {
     tMSS file_map;
     cache_config config;
@@ -64,7 +64,7 @@ void construct(t_index& idx, std::string file, uint8_t num_bytes=0)
 }
 
 template<class t_index, class t_data>
-void construct_im(t_index& idx, t_data data, uint8_t num_bytes=0)
+void construct_im(t_index& idx, const t_data &data, uint8_t num_bytes=0)
 {
     std::string tmp_file = ram_file_name(util::to_string(util::pid())+"_"+util::to_string(util::id()));
     store_to_file(data, tmp_file);
@@ -97,7 +97,7 @@ void construct(t_index& idx, const std::string& file, cache_config& config, uint
     auto event = memory_monitor::event("construct wavelet tree");
     if ((t_index::alphabet_category::WIDTH==8 and num_bytes <= 1)
         or (t_index::alphabet_category::WIDTH==0 and num_bytes != 'd')) {
-        int_vector_buffer<t_index::alphabet_category::WIDTH> text_buf(file, std::ios::in, 1024*1024, num_bytes*8, (bool)num_bytes);
+        int_vector_buffer<> text_buf(file, std::ios::in, 1024*1024, num_bytes*8, (bool)num_bytes);
         t_index tmp(text_buf, text_buf.size());
         idx.swap(tmp);
     } else {
@@ -108,7 +108,7 @@ void construct(t_index& idx, const std::string& file, cache_config& config, uint
         store_to_file(text, tmp_file_name);
         util::clear(text);
         {
-            int_vector_buffer<t_index::alphabet_category::WIDTH> text_buf(tmp_file_name);
+            int_vector_buffer<> text_buf(tmp_file_name);
             t_index tmp(text_buf, text_buf.size());
             idx.swap(tmp);
         }

include/sdsl/wt_pc.hpp
@@ -198,7 +198,8 @@ class wt_pc
          * \par Time complexity
          *      \f$ \Order{n\log|\Sigma|}\f$, where \f$n=size\f$
          */
-        wt_pc(int_vector_buffer<tree_strat_type::int_width>& input_buf,
+        template<uint8_t int_width>
+        wt_pc(int_vector_buffer<int_width>& input_buf,
               size_type size):m_size(size)
         {
             if (0 == m_size)

commit 222250735bd5eb1f8dbb18287c32b9af123a5304

fixed inconsistency introduced in cbcbbcbb9dc1f1a0aa8b89de67fe9e8f1bedc1de

include/sdsl/construct.hpp
@@ -54,7 +54,7 @@ void append_zero_symbol(int_vector& text)
 
 
 template<class t_index>
-void construct(t_index& idx, const std::string &file, uint8_t num_bytes=0)
+void construct(t_index& idx, const std::string& file, uint8_t num_bytes=0)
 {
     tMSS file_map;
     cache_config config;
@@ -65,7 +65,7 @@ void construct(t_index& idx, const std::string &file, uint8_t num_bytes=0)
 }
 
 template<class t_index, class t_data>
-void construct_im(t_index& idx, const t_data &data, uint8_t num_bytes=0)
+void construct_im(t_index& idx, const t_data& data, uint8_t num_bytes=0)
 {
     std::string tmp_file = ram_file_name(util::to_string(util::pid())+"_"+util::to_string(util::id()));
     store_to_file(data, tmp_file);
@@ -98,7 +98,7 @@ void construct(t_index& idx, const std::string& file, cache_config& config, uint
     auto event = memory_monitor::event("construct wavelet tree");
     if ((t_index::alphabet_category::WIDTH==8 and num_bytes <= 1)
         or (t_index::alphabet_category::WIDTH==0 and num_bytes != 'd')) {
-        int_vector_buffer<> text_buf(file, std::ios::in, 1024*1024, num_bytes*8, (bool)num_bytes);
+        int_vector_buffer<t_index::alphabet_category::WIDTH> text_buf(file, std::ios::in, 1024*1024, num_bytes*8, (bool)num_bytes);
         t_index tmp(text_buf, text_buf.size());
         idx.swap(tmp);
     } else {
@@ -109,7 +109,7 @@ void construct(t_index& idx, const std::string& file, cache_config& config, uint
         store_to_file(text, tmp_file_name);
         util::clear(text);
         {
-            int_vector_buffer<> text_buf(tmp_file_name);
+            int_vector_buffer<t_index::alphabet_category::WIDTH> text_buf(tmp_file_name);
             t_index tmp(text_buf, text_buf.size());
             idx.swap(tmp);
         }

include/sdsl/int_vector.hpp
@@ -595,6 +595,8 @@ class int_vector
         {
             uint64_t written_bytes = write_member(size, out);
             if (0 == t_width) {
+                // FYI: int_vector_buffer<> can't read from int_vector_buffer<width>
+                // fix this by always writing the width?
                 written_bytes += write_member(int_width, out);
             }
             return written_bytes;